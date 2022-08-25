404371dc32b66748a5e9483bd5413a3269022fcb
TomJensWilliams <tomjenswilliams@gmail.com>
2022-08-25 16:14:44 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+document.writeln('<div id="w3"></div>');
 let div = document.getElementById("w3");
 let divInnerHTML = `<a href="notesOnW3schoolsHTML.html">HTML</a>`;
 divInnerHTML += `<a href="notesOnW3schoolsCSS.html">CSS</a>`;
