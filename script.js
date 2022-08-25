fc26d80489ccd6f426b28332ac4dd8f08b7cab8d
TomJensWilliams <tomjenswilliams@gmail.com>
2022-08-25 15:59:02 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+let div = document.getElementById("w3");
+let divInnerHTML = `<a href="notesOnW3schoolsHTML.html">HTML</a>`;
+divInnerHTML += `<a href="notesOnW3schoolsCSS.html">CSS</a>`;
+divInnerHTML += `<a href="notesOnW3schoolsJavaScript.html">JavaScript</a>`;
+div.innerHTML = divInnerHTML;
